db916f6d687bb50c3b96ce0762cdf57563815730
kebohan ke <kebohan1@gmail.com>
2019-05-20 00:32:08 +0800

    check error

--- app/Http/Controllers/GPSController.php
+++ app/Http/Controllers/GPSController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
 class GPSController extends Controller
 {
     /**
@@ -13,7 +13,7 @@ class GPSController extends Controller
     {
         //
     }
-    public function get()
+    public function get(Request $request)
     {
         echo '123';
     }
