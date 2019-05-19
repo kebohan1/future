9bc5b6011da33c7b53566fe55dc45a39d5439c98
kebohan ke <kebohan1@gmail.com>
2019-05-20 00:33:35 +0800

    check error

--- app/Http/Controllers/GPSController.php
+++ app/Http/Controllers/GPSController.php
@@ -15,7 +15,9 @@ class GPSController extends Controller
     }
     public function get(Request $request)
     {
-        var_dump($request);
+        if (DB::select('select * from api_token where token = ? and active = true', [$request->token])==null) {
+            return response()->json(['data_status'=>'forbidden:token error']);
+        }
     }
     // public function get(){
     //     var_dump($response);
