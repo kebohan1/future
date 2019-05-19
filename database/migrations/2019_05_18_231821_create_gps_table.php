388008755084aedb6c830fc0f2d7447322bec54c
kebohan ke <kebohan1@gmail.com>
2019-05-19 15:08:14 +0800

    first commit

---
+++ database/migrations/2019_05_18_231821_create_gps_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gps', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->float('longitude');//經度
+            $table->float('latitude');//緯度
+            $table->unsignedInteger('uid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gps');
+    }
+}
