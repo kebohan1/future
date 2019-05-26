c40b916d03aa7093b12e0479aae709c9e53d6754
kebohan ke <kebohan1@gmail.com>
2019-05-27 01:29:49 +0800

    modify NotificationController

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -16,38 +16,39 @@ class NotificationController extends Controller
     }
 
     public function new_notification($response){
+        var_dump($response);
         // if(!(isset($response->name) and isset($response->alarm_time) and isset($response->monday_switch)and isset($response->tuesday_switch) and isset($response->wednesday_switch) and isset($response->thursday_switch) and isset($response->friday_switch) and isset($response->saturday_switch) and isset($response->sunday_switch) and isset($response->active) and isset($response->uid))){
         //     return response()->json(['data_status'=>'some_parameter_is_null'],200);
         // }
-        if (sizeof(DB::table('apitoken')->where('token','=',$response->token)->where('active','=','1')->get())==0) {
-            return response()->json(['data_status'=>'forbidden:token error'],401);
-        }
-        $user = DB::table('users')->where('id','=',$response->uid)->get();
-        // var_dump(Hash::check($response->password,$user[0]->password));
+        // if (sizeof(DB::table('apitoken')->where('token','=',$response->token)->where('active','=','1')->get())==0) {
+        //     return response()->json(['data_status'=>'forbidden:token error'],401);
+        // }
+        // $user = DB::table('users')->where('id','=',$response->uid)->get();
+        // // var_dump(Hash::check($response->password,$user[0]->password));
 
 
-        if(!Hash::check($response->password,$user[0]->password)||isset($user)){
-            return response()->json(['data_status'=>'forbidden:auth error'],403);
-        } else {
-            DB::table('notification')->insert([
+        // if(!Hash::check($response->password,$user[0]->password)||isset($user)){
+        //     return response()->json(['data_status'=>'forbidden:auth error'],403);
+        // } else {
+        //     DB::table('notification')->insert([
             
-                'name' => $response->name, 
-                'alarm_time' => $response->alarm_time,
-                'monday_switch' => $response->monday_switch,
-                'tuesday_switch' => $response->tuesday_switch,
-                'wednesday_switch' => $response->wednesday_switch,
-                'thursday_switch' => $response->thursday_switch,
-                'friday_switch' => $response->friday_switch,
-                'saturday_switch' => $response->saturday_switch,
-                'sunday_switch' => $response->sunday_switch,
-                'active' => '1',
-                'uid' =>$response->uid
+        //         'name' => $response->name, 
+        //         'alarm_time' => $response->alarm_time,
+        //         'monday_switch' => $response->monday_switch,
+        //         'tuesday_switch' => $response->tuesday_switch,
+        //         'wednesday_switch' => $response->wednesday_switch,
+        //         'thursday_switch' => $response->thursday_switch,
+        //         'friday_switch' => $response->friday_switch,
+        //         'saturday_switch' => $response->saturday_switch,
+        //         'sunday_switch' => $response->sunday_switch,
+        //         'active' => '1',
+        //         'uid' =>$response->uid
 
-            ])
-            return response()->json(['data_status'=>'success'],200);
+        //     ])
+        //     return response()->json(['data_status'=>'success'],200);
 
             
-        }
+        // }
     }
     // public function new_user($response){
     //     if(DB::select('select * from users where id = ?',[$response->uid])!=null){
