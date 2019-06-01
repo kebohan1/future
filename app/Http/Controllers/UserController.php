58ab41a938d0011431d8d02af56793cf8e46ef04
kebohan ke <kebohan1@gmail.com>
2019-06-02 04:02:52 +0800

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
 
     public function new_user(Request $response){
         // echo $response;
-        if(User::where('id','=',$response->uid))!=null){
+        if(User::where('id','=',$response->uid)->get())!=null){
             return response()->json(['user_status'=>'exists']);
         } else {
             // $user = new User;
