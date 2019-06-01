8b69f60a9134c266ba7f09bda0ccf8dd16319558
kebohan ke <kebohan1@gmail.com>
2019-06-02 04:13:47 +0800

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,18 +20,19 @@ class UserController extends Controller
 
     public function new_user(Request $response){
         // echo $response;
-        if(!User::where('id','=',$response->uid)->isEmpty()){
-            return response()->json(['user_status'=>'exists']);
-        } else {
-            // $user = new User;
-            // $user->id=$response->uid;
-            // if($request->has('name')){
-            //     $user->name=$response->name;
-            // }
-            // $user->password = Hash::make($response->password);
-            // return response()->json(['user_status'=>'success']);
-            echo "i am in";
-        }
+        $user = User::where('id','=',$response->uid)->isEmpty();
+        // if(!User::where('id','=',$response->uid)->isEmpty()){
+        //     return response()->json(['user_status'=>'exists']);
+        // } else {
+        //     // $user = new User;
+        //     // $user->id=$response->uid;
+        //     // if($request->has('name')){
+        //     //     $user->name=$response->name;
+        //     // }
+        //     // $user->password = Hash::make($response->password);
+        //     // return response()->json(['user_status'=>'success']);
+        //     echo "i am in";
+        // }
     }
 
     // public function update_user_password(Request $response){
