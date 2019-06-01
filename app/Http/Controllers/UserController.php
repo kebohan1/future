390ee295f29c61f941eec19846a54daadd2a78d1
kebohan ke <kebohan1@gmail.com>
2019-06-02 04:50:14 +0800

    update user password update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
     }
 
     public function update_password(Request $response){
-        if (!($response->has('uid') and $response->has('old_password') and $response->has('new_password')) or !empty(User::where('id','=',$response->uid)->first()) ) {
+        if (!($response->has('uid') and $response->has('old_password') and $response->has('new_password')) or empty(User::where('id','=',$response->uid)->first()) ) {
             echo "in";
             abort(404);
         }
